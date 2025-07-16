29c29b5204690542ac483dbafb6be551345c9d96
hortonyyx <65713218+hortonyyx@users.noreply.github.com>
2025-07-16 17:41:46 +0800

    7.16_pra

---
+++ 速通/7.16_列表_pra.py
@@ -0,0 +1,59 @@
+# import random
+#
+# items = [0] * 6
+#
+# for i in range(6000):
+#     face = random.randint(1, 6)
+#     items[face - 1] += 1
+#
+# for _ in range(1,7):
+#     print(f"{_}出现了{items[_-1]}次")
+
+# scores = []
+#
+# for _ in range(5):
+#     temp = []
+#     for _ in range(3):
+#         temp.append(int(input("分数= ")))
+#     scores.append(temp)
+# print(scores)
+
+
+import rich
+
+"""
+双色球随机选号程序
+
+Author: 骆昊
+Version: 1.3
+"""
+import random
+
+from rich.console import Console
+from rich.table import Table
+
+# 创建控制台
+console = Console()
+
+n = int(input('生成几注号码: '))
+red_balls = [i for i in range(1, 34)]
+blue_balls = [i for i in range(1, 17)]
+
+# 创建表格并添加表头
+table = Table(show_header=True)
+for col_name in ('序号', '红球', '蓝球'):
+    table.add_column(col_name, justify='center')
+
+for i in range(n):
+    selected_balls = random.sample(red_balls, 6)
+    selected_balls.sort()
+    blue_ball = random.choice(blue_balls)
+    # 向表格中添加行（序号，红色球，蓝色球）
+    table.add_row(
+        str(i + 1),
+        f'[red]{" ".join([f"{ball:0>2d}" for ball in selected_balls])}[/red]',
+        f'[blue]{blue_ball:0>2d}[/blue]'
+    )
+
+# 通过控制台输出表格
+console.print(table)
---
+++ 速通/7.16_集合_pra.py
@@ -0,0 +1,18 @@
+# set1 = {1, 2, 3, 3, 3, 2}
+# print(set1)
+#
+# set2 = {'banana', 'pitaya', 'apple', 'apple', 'banana', 'grape'}
+# print(set2)
+#
+# set3 = set('hello')
+# print(set3)
+#
+# set4 = set([1, 2, 2, 3, 3, 3, 2, 1])
+# print(set4)
+#
+# set5 = {num for num in range(1, 20) if num % 3 == 0 or num % 7 == 0}
+# print(set5)
+
+set1 = {'Python', 'C++', 'Java', 'Kotlin', 'Swift'}
+for elem in set1:
+    print(elem)
